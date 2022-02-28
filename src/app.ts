34510e9a582ca765476455aa4c1f2c98c8643815
kraccoon-dev <kraccoon0420@gmail.com>
2022-02-28 22:41:58 +0900

    google oauth setup

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,14 @@
 import express, {Application, Request, Response} from 'express'
+import 'dotenv/config'
 
 const app: Application = express()
 
 app.get('/', (req: Request, res: Response) => {
-  res.send('hi there')
+  res.send('<a href="/">login with google</a>')
+})
+
+app.get('/auth/google', (req: Request, res: Response) => {
+  res.send('google oauth callback')
 })
 
 app.listen(3000)
