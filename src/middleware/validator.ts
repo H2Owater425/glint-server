0503520ff701b3baa610dfa813345a2050829ca0
H2Owater425 <h2o@h2owr.xyz>
2022-04-03 20:39:50 +0900

    style: use if/return pattern instead

--- src/middleware/validator.ts
+++ src/middleware/validator.ts
@@ -8,32 +8,34 @@ export default function (type: any): RequestHandler {
   return (request: Request, response: unknown, next: NextFunction): void => {
     const body = request.body
 
-    if (typeof body !== 'undefined') {
-      validate(plainToInstance(type, body)).then(
-        (errors: ValidationError[]): void => {
-          if (errors.length === 0) {
-            next()
-
-            return
-          }
-
-          const exceptions: string[] = []
-
-          for (let i = 0; i < errors.length; i++) {
-            exceptions.push(Object.values(errors[i].constraints)[0])
-          }
-
-          next(
-            new HttpException(400, 'missing or invalid data', {
-              errors: exceptions,
-            })
-          )
-
-          return
-        }
-      )
-    } else {
+    if (typeof body === 'undefined') {
       next(new HttpException(400, 'no passed data'))
-    }
+
+			return
+		}
+
+		validate(plainToInstance(type, body)).then(
+			(errors: ValidationError[]): void => {
+				if (errors.length === 0) {
+					next()
+
+					return
+				}
+
+				const exceptions: string[] = []
+
+				for (let i = 0; i < errors.length; i++) {
+					exceptions.push(Object.values(errors[i].constraints)[0])
+				}
+
+				next(
+					new HttpException(400, 'missing or invalid data', {
+						errors: exceptions,
+					})
+				)
+
+				return
+			}
+		)
   }
 }
