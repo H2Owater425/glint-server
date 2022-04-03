cc93c490ef60b0c122fef20725dc4765d618e5b7
H2Owater425 <h2o@h2owr.xyz>
2022-04-03 15:42:17 +0900

    style: fix typo
    style: fix validation logic

--- src/routers/register/addUser.controller.ts
+++ src/routers/register/addUser.controller.ts
@@ -1,7 +1,7 @@
 import { Request, Response, NextFunction } from 'express'
 import { getFirestore } from 'firebase-admin/firestore'
 import { v5 as uuidv5 } from 'uuid'
-import { isIdExists, isEmailExists } from '@lib/exist'
+import { isExistingId, isExistingEmail } from '@lib/exist'
 import HttpException from '@exceptions/http'
 import UserDto from './user.dto'
 import { createHash, randomBytes } from 'crypto'
@@ -16,28 +16,28 @@ export default async function (
   const uuid: string = uuidv5(body.email, uuidv5.URL)
 
   try {
-    if (!(await isEmailExists(uuid))) {
-      if (!(await isIdExists(body.id))) {
-        body.salt = randomBytes(128).toString('base64')
-
-				while(body.salt.charAt(body.salt.length - 1) === '=') {
-					body.salt = body.salt.slice(0, -1)
-				}
-
-        body.password = createHash('sha256')
-          .update(body.password + '+' + body.salt)
-          .digest()
-          .toString('hex')
-
-        await getFirestore().collection('users').doc(uuid).set(body)
-
-        response.json({ message: 'sucess' })
-      } else {
-        throw new HttpException(400, 'existing id')
-      }
-    } else {
+    if (await isExistingId(uuid)) {
       throw new HttpException(400, 'existing email')
     }
+
+    if (await isExistingEmail(body.id)) {
+      throw new HttpException(400, 'existing id')
+    }
+
+    body.salt = randomBytes(128).toString('base64')
+
+    while (body.salt.charAt(body.salt.length - 1) === '=') {
+      body.salt = body.salt.slice(0, -1)
+    }
+
+    body.password = createHash('sha256')
+      .update(body.password + '+' + body.salt)
+      .digest()
+      .toString('hex')
+
+    await getFirestore().collection('users').doc(uuid).set(body)
+
+    response.json({ message: 'sucess' })
   } catch (error: any) {
     console.log(error)
 
