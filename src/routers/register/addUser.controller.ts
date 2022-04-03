54ac897e995ba210575ee27faa7e9a9e0f2a6b90
H2Owater425 <h2o@h2owr.xyz>
2022-04-03 10:37:43 +0900

    feat: implement salt in user add handler

--- src/routers/register/addUser.controller.ts
+++ src/routers/register/addUser.controller.ts
@@ -3,33 +3,50 @@ import { getFirestore } from 'firebase-admin/firestore'
 import { v5 as uuidv5 } from 'uuid'
 import { isIdExists, isEmailExists } from '@lib/exist'
 import HttpException from '@exceptions/http'
+import UserDto from './user.dto'
+import { createHash, randomBytes } from 'crypto'
 
-export default async function addUser(
-  req: Request,
-  res: Response,
+// addUser
+export default async function (
+  request: Request<UserDto>,
+  response: Response,
   next: NextFunction
-) {
-  const body = req.body
-  const uuid = uuidv5(body.email, uuidv5.URL)
-  const db = getFirestore()
-  const userRef = db.collection('users')
-  const userDoc = userRef.doc(uuid)
+): Promise<void> {
+  const body: UserDto & { salt: string } = request.body
+  const uuid: string = uuidv5(body.email, uuidv5.URL)
 
   try {
-    if (await isIdExists(uuid)) {
-      next(new HttpException(400, 'existing email'))
-      return
-    }
+    if (!(await isEmailExists(uuid))) {
+      if (!(await isIdExists(body.id))) {
+        body.salt = randomBytes(128).toString('base64')
 
-    if (await isEmailExists(body.id)) {
-      next(new HttpException(400, 'existing id'))
-      return
-    }
+				while(body.salt.charAt(body.salt.length - 1) === '=') {
+					body.salt = body.salt.slice(0, -1)
+				}
 
-    await userDoc.set({ ...body }) // save data
-    res.json({ message: 'sucess' })
-  } catch (error) {
+        body.password = createHash('sha256')
+          .update(body.password + '+' + body.salt)
+          .digest()
+          .toString('hex')
+
+        await getFirestore().collection('users').doc(uuid).set(body)
+
+        response.json({ message: 'sucess' })
+      } else {
+        throw new HttpException(400, 'existing id')
+      }
+    } else {
+      throw new HttpException(400, 'existing email')
+    }
+  } catch (error: any) {
     console.log(error)
-    next(new HttpException(500, 'server error'))
+
+    next(
+      error instanceof HttpException
+        ? error
+        : new HttpException(500, 'server error')
+    )
   }
+
+  return
 }
