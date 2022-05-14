b503951b208154bac7667745ffd5549275f704de
H2Owater425 <h2o@h2owr.xyz>
2022-05-14 11:42:18 +0900

    feat: add routers

--- src/routers/index.ts
+++ src/routers/index.ts
@@ -1,4 +1,5 @@
-import register from './register'
+import users from './users'
 import root from './root'
+import auth from './auth'
 
-export default [root, register]
+export default [root, users, auth]
